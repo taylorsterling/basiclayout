aaeb20e5236e4f1a8d5848faf5cb95f28c81a9fc
Taylor <taylorsterling@icloud.com>
2019-08-27 23:01:28 -0500

    updating port

--- server.js
+++ server.js
@@ -8,6 +8,6 @@
  res.sendFile(path.join(__dirname+'/dist/basiclayout/index.html'));
  });
  // Start the app by listening on the default Heroku port
- app.listen(process.env.PORT || 8080);
+ app.listen(process.env.PORT || 4200);
 
  
